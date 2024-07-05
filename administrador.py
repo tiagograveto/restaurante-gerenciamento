af0d2e025796f1fa9a36cd44fc49c0c5f73133f6
Tiago Graveto <tiagograveto@outlook.com>
2024-07-05 15:44:22 -0300

    Adm finished

--- administrador.py
+++ administrador.py
@@ -4,6 +4,9 @@ from PySide6.QtWidgets import QMessageBox
 
 from modelo import PEDIDOS, ItemStatus
 
+QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts, True)
+
+
 class Administrador:
 
     pedidoToDeliver = []
@@ -26,7 +29,7 @@ class Administrador:
     def carregarBotoes(self):
         self._interface.homeButton.clicked.connect(self.aoClicarHomeButton)
         self._interface.moverButton.clicked.connect(self.aoClicarMoverButton)
-        self._interface.relatorioButton.clicked.connect(lambda x:self._interface.pages.setCurrentWidget(self._interface.page_2))
+        self._interface.relatorioButton.clicked.connect(self.aoClicarRelatorioButton)
 
         self._interface.ordersDelivered2.itemClicked.connect(self.aoClicarItemDelivered)
         self._interface.ordersPreparation2.itemClicked.connect(self.aoClicarItemPreparation)
@@ -42,6 +45,7 @@ class Administrador:
     
     def aoClicarRelatorioButton(self):
         self._interface.relatorioButton.clicked.connect(lambda x: self._interface.pages.setCurrentWidget(self._interface.page_2))
+        self.atualizarPagRelatorio()
 
 
     def getPedidosPorItemStatus(self, itemstatus):
@@ -71,14 +75,27 @@ class Administrador:
         pedidoIndex = clickedItem.data(QtCore.Qt.UserRole)
         self.pedidoToDeliver.append(pedidoIndex)
         self._interface.ordersDelivered2.addItem(widgetItem)
+        index = self._interface.ordersPreparation2.row(clickedItem)
+        self._interface.ordersPreparation2.takeItem(index)
+
     
     def aoClicarItemDelivered(self):
         index = self._interface.ordersDelivered2.currentRow()
         if index >= 0:
+            itemText = self._interface.ordersDelivered2.item(index).text()
+            itemData = self._interface.ordersDelivered2.item(index).data(QtCore.Qt.UserRole)
+
             item = self._interface.ordersDelivered2.takeItem(index)
+            
+            newItem = QtWidgets.QListWidgetItem(itemText)
+            newItem.setData(QtCore.Qt.UserRole, itemData)
+
+            self._interface.ordersPreparation2.addItem(newItem)
+            
             self.pedidoToDeliver.pop(index)
+            
             del item
-    
+       
     def aoClicarDeliveredButton(self):
         pedidosToDeliver = self.pedidoToDeliver
         if(pedidosToDeliver == []):
@@ -90,7 +107,8 @@ class Administrador:
         else:
             for i in range(len(pedidosToDeliver)):
                 PEDIDOS[i].deliver_items()
-
+            
+            self.pedidoToDeliver.clear()
             self.atualizarPagMover()
 
     def atualizarPagHome(self):
@@ -107,3 +125,48 @@ class Administrador:
         for pedido in inDelivered:
             widgetItem = QtWidgets.QListWidgetItem(pedido.get_pedido())
             self._interface.ordersDelivered.addItem(widgetItem)
+
+
+    def atualizarPagRelatorio(self):
+
+        sold_items = self.getPedidosPorItemStatus(None)
+
+        item_counts = {}
+        for pedido in sold_items:
+            for item in pedido.get_items():
+                item_name = item.get_name()  
+                if item_name in item_counts:
+                    item_counts[item_name] += 1
+                else:
+                    item_counts[item_name] = 1
+
+        self._interface.listWidget.clear()
+
+        for item_name, count in item_counts.items():
+            widgetItem = QtWidgets.QListWidgetItem(f"{item_name} - {count}")
+            self._interface.listWidget.addItem(widgetItem)
+            self.atualizarFaturamento()
+            self.atulizarDeliveredItems()
+           
+
+    def atualizarFaturamento(self):
+        
+        sold_items = self.getPedidosPorItemStatus(None)
+
+        total = 0
+        for pedido in sold_items:
+            for item in pedido.get_items():
+                total += item.get_price()
+
+        self._interface.label_7.setText(f"R$ {total:.2f}")
+
+    def atulizarDeliveredItems(self):
+        delivered_items = self.getPedidosPorItemStatus(ItemStatus.DELIVERED)
+
+        count_delivered = 0
+        for pedido in delivered_items:
+            count_delivered += 1
+
+        self._interface.label_6.setText(f"{count_delivered}")
+
+            
